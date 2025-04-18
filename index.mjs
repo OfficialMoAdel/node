6d6535811a5a3ce2c3b8ecf154cc5ee06e6be332
OfficialMoAdel <mohamed01023618534@gmail.com>
2025-04-18 19:27:08 +0200

    firest comment

---
+++ index.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+import os from "os";
+
+const app = express();
+const PORT = 3000
+
+app.get("/", (req, res) => {
+ const massage = `Hello from ${os.hostname()}`
+ res.send(massage)
+})
+app.listen(PORT, () => {
+ console.log(`webserver is listening on port ${PORT}`)
+    console.log(os.hostname())
+})
